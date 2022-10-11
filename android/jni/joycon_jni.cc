4107adfb2da7622c2b513d527fe4627238d8d46d
Abandoned Cart <twistedumbrella@gmail.com>
2022-10-11 00:22:23 -0400

    Update licensing

--- android/jni/joycon_jni.cc
+++ android/jni/joycon_jni.cc
@@ -1,6 +1,6 @@
 /*
  *   Copyright (c) 2020 mumumusuc
- *   Copyright (c) 2022 AbandonedCart.  All rights reserved.
+ *   Copyright (c) 2022 AbandonedCart
 
  *   This program is free software: you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
